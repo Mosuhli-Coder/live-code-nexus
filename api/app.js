326757e4db5a2f31f178203c8bd92de3bc573208
Teboho Mosuhli <drmosuhli@gmail.com>
2024-01-17 12:08:45 +0200

    add pre installation for deployment

--- api/app.js
+++ api/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import path from 'path'
 
+const __dirname = path.resolve();
 const app = express();
 
 
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 app.listen(3000, () => {
     console.log('Server running on port 3000!!!!');
 })
