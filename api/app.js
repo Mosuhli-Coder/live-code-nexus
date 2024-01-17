cd2d0d5901eaa486e1f59dde7bb8783a6a9c4c36
Teboho Mosuhli <drmosuhli@gmail.com>
2024-01-17 23:30:52 +0200

    connect to mongodb database

--- api/app.js
+++ api/app.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 import path from 'path'
+
+
+mongoose.connect(process.env.MONGO_DB).then(()=> {
+    console.log('Connected to MongoDB!');
+}).catch((err)=>{
+    console.log(err);
+})
 
 const __dirname = path.resolve();
 const app = express();
