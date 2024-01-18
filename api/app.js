247c9b48f196ca7722ad26d09d78033eda72859c
Teboho Mosuhli <drmosuhli@gmail.com>
2024-01-18 10:44:31 +0200

    complete sign in functionality

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_DB).then(()=> {
 const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
