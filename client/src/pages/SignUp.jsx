27c7bdb5d65949ee6055b0cb50ea555bdd166975
Teboho Mosuhli <drmosuhli@gmail.com>
2024-01-18 08:37:34 +0200

    complete create account UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Create account</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text"  placeholder='name' className='vorder p-3 rounded-lg' id='firstName' />
+        <input type="text"  placeholder='surname' className='vorder p-3 rounded-lg' id='surname' />
+        <input type="email"  placeholder='email' className='vorder p-3 rounded-lg' id='email' />
+        <input type="password"  placeholder='password' className='vorder p-3 rounded-lg' id='password' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase
+        hover:opacity-95 disabled:opacity-80'>Create Account</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+    </div>
   )
 }
